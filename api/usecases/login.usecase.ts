14ff227d0ae99a0b5bda9b08d9f6203da6c957c7
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 21:24:53 +0100

    fix: returns only the response object

--- api/usecases/login.usecase.ts
+++ api/usecases/login.usecase.ts
@@ -1,14 +1,10 @@
 import { api } from "../axios-instance";
-import { z } from "zod";
-import { loginSchema } from "../../schemas/login.schema";
 
-type LoginUseCaseProps = z.infer<typeof loginSchema>;
-
-export const loginUseCase = async ({ number, password }: LoginUseCaseProps) => {
+export const loginUseCase = async ({ number, password }) => {
   const response = api.post("/auth/login", {
     number,
     password,
   });
 
-  return (await response).data;
+  return response;
 };
