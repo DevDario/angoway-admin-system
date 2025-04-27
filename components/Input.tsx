8d4899f10f9c05432115a69a1fbad644a095368a
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 20:41:29 +0100

    feat: new input component

---
+++ components/Input.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import "./Input.css"
+
+type InputProps = {
+  name: string;
+  control: any;
+  placeholder: string;
+  error?: string;
+  type: string;
+};
+
+const Input: React.FC<InputProps> = ({
+  name,
+  control,
+  placeholder,
+  error,
+  type,
+}) => {
+  return (
+    <div className="input-wrapper">
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <input
+            {...field}
+            placeholder={placeholder}
+            type={type}
+            className={`input ${error ? "input-error" : ""}`}
+          />
+        )}
+      />
+      {error && <span className="input-error-message">{error}</span>}
+    </div>
+  );
+};
+
+export default Input;
