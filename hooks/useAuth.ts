0405e2f7dfcbb27202fec7276ecb818a5ad7820a
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 17:31:03 +0100

    feat: creates a custom hook for login with react query mutations

---
+++ hooks/useAuth.ts
@@ -0,0 +1,55 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { loginUseCase } from "../api/usecases/login.usecase";
+import { useEffect, useState } from "react";
+import { getToken, saveToken, removeToken } from "../utils/secure-store";
+import { LoginResponse } from "../types/login.response";
+
+export function useAuth() {
+  const queryClient = useQueryClient();
+  const [authToken, setAuthToken] = useState<string | null>(null);
+  const [isCheckingAuth, setIsCheckingAuth] = useState<boolean>(true);
+  const [authError, setAuthError] = useState<string | null>(null);
+
+  useEffect(() => {
+    function checkToken() {
+      const token: string | null | undefined = getToken();
+      setAuthToken(token || null);
+      setIsCheckingAuth(false);
+    }
+    checkToken();
+  }, []);
+
+  const useLogin = useMutation({
+    mutationFn: loginUseCase,
+    onMutate: () => {
+      setIsCheckingAuth(true);
+      setAuthError(null);
+    },
+    onSuccess: async (data: LoginResponse) => {
+      saveToken(data.access_token);
+      setAuthToken(data.access_token);
+      //   navigate to dashboard page
+      queryClient.invalidateQueries();
+    },
+    onError: (error) => {
+      setAuthError(error.message);
+    },
+    onSettled: () => {
+      setIsCheckingAuth(false);
+    },
+  });
+
+  const logout = async () => {
+    removeToken();
+    // navigato to login page
+    queryClient.clear();
+  };
+
+  return {
+    useLogin,
+    logout,
+    authToken,
+    isCheckingAuth,
+    authError,
+  };
+}
