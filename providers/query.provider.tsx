8b2ac1df5f93c705e8febeb5311ea7336679b5d0
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 17:29:21 +0100

    feat: creates a global query provider

---
+++ providers/query.provider.tsx
@@ -0,0 +1,9 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useState, ReactNode } from "react";
+
+export default function QueryProvider({ children }: { children?: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
