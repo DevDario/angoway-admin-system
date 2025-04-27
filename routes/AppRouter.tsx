f58497d30873d3d09e60cbdf437cb7aaf6902b16
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 18:08:47 +0100

    feat: handles authentication routes

---
+++ routes/AppRouter.tsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import LoginPage from "../pages/login/LoginPage";
+import DashboardPage from "../pages/dashboard/DashboardPage";
+
+export default function AppRouter() {
+  const { authToken } = useAuth();
+
+  return (
+    <Routes>
+      {!authToken ? (
+        <>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </>
+      )}
+    </Routes>
+  );
+}
