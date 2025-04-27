d4dc91d0d60a5aa9e5c56cd4b28de355b4806552
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 20:50:26 +0100

    feat: new modal component

---
+++ components/AlertModal.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWarning, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "./AlertModal.css";
+import Button from "./Button";
+
+interface AlertModalProps {
+  text: string;
+  type: "error" | "warning";
+}
+
+const AlertModal: React.FC<AlertModalProps> = ({ text, type }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <div className={`modal-container ${isVisible ? "visible" : "hidden"}`}>
+      <div className="modal-content">
+        <div className={`icon-container ${type}`}>
+          <FontAwesomeIcon
+            icon={type === "error" ? faTimes : faWarning}
+            color="#FFF"
+            width={30}
+            height={30}
+          />
+        </div>
+        <p className="modal-text">{text}</p>
+        <Button text="Fechar" onClick={() => setIsVisible(false)} />
+      </div>
+    </div>
+  );
+};
+
+export default AlertModal;
