21b748c589c13d05033a758a25c90c306d0521da
Dário Silva <dariosilva13222@gmail.com>
2025-04-27 18:08:27 +0100

    feat: implement authentication loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAuth } from "../hooks/useAuth";
+import { BrowserRouter } from "react-router";
+import AppRouter from "../routes/AppRouter"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { isCheckingAuth } = useAuth();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  if (isCheckingAuth) {
+    return (
+      <div style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <p>Carregando...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
+
+  return (<BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>);
 }
 
-export default App
+export default App;
