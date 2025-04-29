0ccd20cc1bd3225168103fefe62cc30d9fca5589
Dário Silva <dariosilva13222@gmail.com>
2025-04-30 00:56:50 +0100

    feat: creates a sections header component to place the title of a section

---
+++ components/SectionHeader.tsx
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import "./SectionHeader.css";
+
+type sectionHeaderProps = {
+  icon?: IconProp;
+  title: string;
+};
+
+export default function SectionHeader({ icon, title }: sectionHeaderProps) {
+  return (
+    <div className="header-container">
+      <div className="header-container-icon-box">
+        <FontAwesomeIcon icon={icon} width={12} height={12} />
+      </div>
+      <h1 className="header-container-title">{title}</h1>
+    </div>
+  );
+}
