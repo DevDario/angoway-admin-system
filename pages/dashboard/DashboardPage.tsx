efae9c2460127ede4527ca7c9a381f9953e3e711
Dário Silva <dariosilva13222@gmail.com>
2025-04-30 00:32:47 +0100

    feat:implements the chart into the dashboard

--- pages/dashboard/DashboardPage.tsx
+++ pages/dashboard/DashboardPage.tsx
@@ -2,24 +2,58 @@ import React from "react";
 import DashboardDataCard from "../../components/DashboardDataCard";
 import Layout from "../_layout";
 import "./DashboardPage.css";
+import Chart from "../../components/Chart";
 import Button from "../../components/Button";
-import {faChevronDown, faBusSimple, faFileExport,faClose, faUser, faQuestionCircle} from "@fortawesome/free-solid-svg-icons"
+import {
+  faChevronDown,
+  faBusSimple,
+  faFileExport,
+  faClose,
+  faUser,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function DashboardPage() {
   return (
     <Layout>
       <div className="content-container">
         <div className="data-summarization-container">
-          <DashboardDataCard label="Autocarros Ativos" value={34} icon={faBusSimple}/>
-          <DashboardDataCard label="Autocarros Inativos" value={5} icon={faClose} />
-          <DashboardDataCard label="Motoristas Cadastrados" value={42} icon={faUser}/>
-          <DashboardDataCard label="..." value={10} icon={faQuestionCircle}/>
-          <DashboardDataCard label="..." value={10} icon={faQuestionCircle}/>
+          <DashboardDataCard
+            label="Autocarros Ativos"
+            value={34}
+            icon={faBusSimple}
+          />
+          <DashboardDataCard
+            label="Autocarros Inativos"
+            value={5}
+            icon={faClose}
+          />
+          <DashboardDataCard
+            label="Motoristas Cadastrados"
+            value={42}
+            icon={faUser}
+          />
+          <DashboardDataCard label="..." value={10} icon={faQuestionCircle} />
+          <DashboardDataCard label="..." value={10} icon={faQuestionCircle} />
         </div>
         <div className="chart-container">
           <div className="chart-action-buttons-container">
-            <Button text="Mensal" onClick={() => {}} icon={faChevronDown} iconColor="#FFF"/>
-            <Button text="Exportar" onClick={() => {}} icon={faFileExport} iconColor="#FFF"/>
+            <Button
+              text="Mensal"
+              onClick={() => {}}
+              icon={faChevronDown}
+              iconColor="#FFF"
+            />
+            <Button
+              text="Exportar"
+              onClick={() => {}}
+              icon={faFileExport}
+              iconColor="#FFF"
+            />
+          </div>
+          <div className="chart-box">
+            <h2 className="chart-title">Viagens realizadas</h2>
+            <Chart />
           </div>
         </div>
       </div>
