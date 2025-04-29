f478b0a56ec4249d81b61f1e9f198cafd6c8d207
Dário Silva <dariosilva13222@gmail.com>
2025-04-29 22:39:00 +0100

    feat: renders summarization cards

--- pages/dashboard/DashboardPage.tsx
+++ pages/dashboard/DashboardPage.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import DashboardDataCard from "../../components/DashboardDataCard";
+import Layout from "../_layout";
+import "./DashboardPage.css";
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
-  )
+    <Layout>
+      <div className="content-container">
+        <div className="data-summarization-container">
+          <DashboardDataCard label="Autocarros Ativos" value={34} />
+          <DashboardDataCard label="Autocarros Inativos" value={5} />
+          <DashboardDataCard label="Motoristas Cadastrados" value={42} />
+          <DashboardDataCard label="..." value={10} />
+          <DashboardDataCard label="..." value={10} />
+        </div>
+      </div>
+    </Layout>
+  );
 }
