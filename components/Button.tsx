36d73badce0794a426547bf005e58fa12bcbe3d7
Dário Silva <dariosilva13222@gmail.com>
2025-04-29 23:05:29 +0100

    feat: adds optional icons and icon related props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,34 @@
 import React from "react";
-import "./Button.css"
+import "./Button.css";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type ButtonProps = {
   text: string;
   onClick: () => void;
   disabled?: boolean;
+  icon?: IconProp;
+  iconColor?: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, disabled }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  disabled,
+  icon,
+  iconColor
+}: ButtonProps) => {
   return (
     <button className="button" onClick={onClick} disabled={disabled}>
       {text}
+      {icon && (
+        <FontAwesomeIcon
+          icon={icon}
+          style={{ color: iconColor ? iconColor : "#0C6DFF", marginLeft:"8px"}}
+          width={13}
+          height={13}
+        />
+      )}
     </button>
   );
 };
