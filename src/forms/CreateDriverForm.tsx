ca8dc96959b01fa6eb755afecfe0bc97f8c6b4b0
Dário Silva <dariosilva13222@gmail.com>
2025-05-06 14:50:07 +0100

    feat: creates a custom Form to register drivers using zod and react-hook-form

---
+++ src/forms/CreateDriverForm.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { createDriverSchema } from "@/schemas/driver.create.schema";
+
+export default function CreateDriverForm() {
+  const form = useForm({
+    resolver: zodResolver(createDriverSchema),
+    defaultValues: {
+      name: "",
+      number: "",
+      password: "",
+      email: "",
+      role: "DRIVER",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof createDriverSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem style={{ marginBottom: 15 }}>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="John Doe"
+                  {...field}
+                  style={{
+                    padding: 10,
+                    height: 45,
+                    width: 400,
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem style={{ marginBottom: 15 }}>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="johndoe@gmail.com"
+                  {...field}
+                  style={{
+                    padding: 10,
+                    height: 45,
+                    width: 400,
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="number"
+          render={({ field }) => (
+            <FormItem style={{ marginBottom: 15 }}>
+              <FormLabel>Número</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="9xx xxx xxx"
+                  {...field}
+                  style={{
+                    padding: 10,
+                    height: 45,
+                    width: 400,
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem style={{ marginBottom: 15 }}>
+              <FormLabel>Palavra Passe</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder=""
+                  {...field}
+                  style={{
+                    padding: 10,
+                    height: 45,
+                    width: 400,
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          style={{ height: 45, width: 400, cursor: "pointer", backgroundColor:"#0C6BFF", color:"#FCFCFB" }}
+        >
+          Cadastrar
+        </Button>
+      </form>
+    </Form>
+  );
+}
