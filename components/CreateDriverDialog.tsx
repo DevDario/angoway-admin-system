afa6eb022f7f30b6c5b2663052f8cdcfe80519cb
Dário Silva <dariosilva13222@gmail.com>
2025-05-06 20:55:11 +0100

    feat: creates a custom dialog that presents a form to register new drivers

---
+++ components/CreateDriverDialog.tsx
@@ -0,0 +1,28 @@
+import CreateDriverForm from "@/forms/CreateDriverForm";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../src/components/ui/dialog";
+import React, { ReactNode } from "react";
+import "./CreateDriverDialog.css";
+
+export default function CreateDriverDialog({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[443px]" style={{backgroundColor:"#121212", padding: 20}}>
+        <DialogHeader>
+          <DialogTitle>Criar Motorista</DialogTitle>
+        </DialogHeader>
+        <CreateDriverForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
