5d00b056879f431cc672e55df5aee2076c36ff8f
Dário Silva <dariosilva13222@gmail.com>
2025-05-10 11:44:47 +0100

    feat: creates a socket instance provider

---
+++ api/socket-instance.ts
@@ -0,0 +1,15 @@
+import { io } from "socket.io-client";
+import { getToken } from "../utils/secure-store";
+
+const BACKEND_URL = process.env.VITE_PUBLIC_API_BASE_URL;
+
+const socket = io(BACKEND_URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+  auth: async (cb) => {
+    const token = await getToken();
+    cb({ token });
+  },
+});
+
+export { socket };
