2884b52861150347f3fc5a5ebb20ff1485695ded
Dário Silva <dariosilva13222@gmail.com>
2025-05-12 12:34:33 +0100

    feat: renders all alerts comming from alerts channel and handles alerts selection

--- pages/alerts/AlertsPage.tsx
+++ pages/alerts/AlertsPage.tsx
@@ -1,55 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../_layout";
 import SectionHeader from "../../components/SectionHeader";
-import { faWarning } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faWarning } from "@fortawesome/free-solid-svg-icons";
 import AlertChip from "../../components/AlertChip";
 import "./AlertsPage.css";
-
-const alerts = [
-  {
-    id: 1,
-    message: "Um autocarro teve um acidente",
-    timestamp: new Date().getHours() + ":" + new Date().getMinutes(),
-  },
-  {
-    id: 2,
-    message: "Um autocarro sofreu avaria total",
-    timestamp: new Date().getHours() + ":" + new Date().getMinutes(),
-  },
-  {
-    id: 3,
-    message: "Um autocarro teve um acidente",
-    timestamp: new Date().getHours() + ":" + new Date().getMinutes(),
-  },
-  {
-    id: 4,
-    message: "Um autocarro sofreu avaria total",
-    timestamp: new Date().getHours() + ":" + new Date().getMinutes(),
-  },
-];
+import { useAlertsChannel } from "../../hooks/useAlertsChannel";
+import { AlertNotification } from "../../types/alert.notification";
 
 export default function AlertsPage() {
+  const [visible, setVisible] = useState<boolean>(false);
+  const [previewAlert, setPreviewAlert] = useState<AlertNotification | null>(null);
+  const { alerts } = useAlertsChannel();
+
+  function handleAlertSelect(alert: AlertNotification) {
+    setPreviewAlert(alert);
+    setVisible(true);
+  }
+
   return (
     <Layout>
-      <div className="content-container">
+      <div className="alerts-page-content-container content-container">
         <div className="alerts-container">
           <SectionHeader title="Alertas" icon={faWarning} />
-          {alerts.map((alert) => (
-            <AlertChip
-              id={alert.id}
-              message={alert.message}
-              timestamp={alert.timestamp}
-              onClick={() => null}
-              key={alert.id}
-            />
-          ))}
+          {alerts.length > 0 ? (
+            alerts.map((alert) => (
+              <AlertChip
+                id={alerts.length}
+                message={alert.message}
+                timestamp={alert.timestamp + ""}
+                onClick={() => handleAlertSelect(alert)}
+                key={alert.driverId}
+              />
+            ))
+          ) : (
+            <p>Sem alertas no momento</p>
+          )}
         </div>
         <div className="alert-preview-container">
-          <SectionHeader title="Preview" icon={faWarning} />
+          <SectionHeader title="Preview" icon={faEye} />
           <div className="preview-box">
-            <p className="preview-box-text">
-              Escolhe um alerta para visualizar
-            </p>
+            {visible && previewAlert !== null ? (
+              <p>preview alert</p>
+            ) : (
+              <p className="preview-box-text">
+                Escolhe um alerta para visualizar
+              </p>
+            )}
           </div>
         </div>
       </div>
