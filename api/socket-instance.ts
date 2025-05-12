503c963139d467f6618d4b03e29e85637bea7740
Dário Silva <dariosilva13222@gmail.com>
2025-05-12 12:14:46 +0100

    fix: update BACKEND_URL to use import.meta.env and optimize token retrieval

--- api/socket-instance.ts
+++ api/socket-instance.ts
@@ -1,13 +1,13 @@
 import { io } from "socket.io-client";
 import { getToken } from "../utils/secure-store";
 
-const BACKEND_URL = process.env.VITE_PUBLIC_API_BASE_URL;
+const BACKEND_URL = import.meta.env.API_BASE_URL;
 
 const socket = io(BACKEND_URL, {
   autoConnect: false,
   transports: ["websocket"],
   auth: async (cb) => {
-    const token = await getToken();
+    const token = getToken();
     cb({ token });
   },
 });
