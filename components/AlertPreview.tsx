a24c6f28fc89c3d8344376b78559c886be8c002a
Dário Silva <dariosilva13222@gmail.com>
2025-05-13 19:38:52 +0100

    feat: adds props based on AlertNotification type

--- components/AlertPreview.tsx
+++ components/AlertPreview.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./AlertPreview.css";
+import { AlertNotification } from "types/alert.notification";
 
-type AlertPreviewProps = {
-  type: string;
-  driverId: number;
-  timestamp: number;
-  message: string;
-  location: { lat: number; lng: number };
-};
 
 export default function AlertPreview({
   type,
   driverId,
+  driverName,
   timestamp,
   message,
-  location,
-}: AlertPreviewProps) {
+  loc,
+  busNIA,
+  route,
+}: AlertNotification) {
   return (
     <div className="alert-preview-card-container">
       <div className="alert-type-container">
@@ -25,11 +22,18 @@ export default function AlertPreview({
       </div>
       <div className="emitter-id-container">
         <h1 className="emitter-label preview-label">Emitido Por:</h1>
-        <h2 className="emitter-id-value">{driverId}</h2>
+        <h2 className="emitter-id-value">{`${driverName} (#${driverId})`}</h2>
       </div>
       <div className="timestamp-container">
         <h1 className="timestamp-label preview-label">Emitido em:</h1>
         <h2 className="timestamp-value">{timestamp}</h2>
+      </div>
+      <div className="details-container">
+        <h1 className="details-label preview-label">Detalhes do Autocarro:</h1>
+        <ol style={{ listStyleType: "disc", listStyle: "inside" }}>
+          <li className="details-value">{`NIA: ${busNIA}`}</li>
+          <li className="details-value">{`Rota: ${route}`}</li>
+        </ol>
       </div>
       <div className="message-container">
         <h1 className="message-label preview-label">Mensagem:</h1>
@@ -37,7 +41,7 @@ export default function AlertPreview({
       </div>
       <div className="location-container">
         <h1 className="location-label preview-label">Localização:</h1>
-        <h2 className="location-value">{location.lat + "," + location.lng}</h2>
+        <h2 className="location-value">{loc?.lat + "," + loc?.lng}</h2>
         <div className="map-container"></div>
       </div>
     </div>
