50bb7b24bc0b0fa10118afdc38b4a86429e3311f
Dário Silva <dariosilva13222@gmail.com>
2025-05-13 19:38:19 +0100

    fix: adds missing props

--- types/alert.notification.ts
+++ types/alert.notification.ts
@@ -1,6 +1,9 @@
 export type AlertNotification = {
   type: string;
   driverId?: number;
+  driverName: string;
+  busNIA: string;
+  route: string;
   message: string;
   loc?: { lat: number; lng: number };
   timestamp?: number;
