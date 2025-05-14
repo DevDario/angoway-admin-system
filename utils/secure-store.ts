4c34258bd2a9ec0824db11b1c4665f0a0c376f1d
Dário Silva <dariosilva13222@gmail.com>
2025-05-14 23:24:12 +0100

    feat: add alert management functions for saving, retrieving, and removing alerts

--- utils/secure-store.ts
+++ utils/secure-store.ts
@@ -1,4 +1,7 @@
-const TOKEN_KEY: string = import.meta.env.VITE_EXPO_PUBLIC_TOKEN_KEY || "access_token";
+import { AlertNotification } from "../types/alert.notification";
+
+const TOKEN_KEY: string =
+  import.meta.env.VITE_EXPO_PUBLIC_TOKEN_KEY || "access_token";
 
 export function saveToken(token: string) {
   try {
@@ -22,5 +25,47 @@ export function removeToken() {
     localStorage.removeItem(TOKEN_KEY);
   } catch (error) {
     console.error("Error removing token:", error);
+  }
+}
+
+export function saveAlert(alert: AlertNotification) {
+  try {
+    const alerts: AlertNotification[] = JSON.parse(
+      localStorage.getItem("alerts") || "[]"
+    );
+    const index = alerts.findIndex((a) => a.driverId === alert.driverId);
+    if (index !== -1) {
+      alerts[index] = alert;
+    } else {
+      alerts.push(alert);
+    }
+    localStorage.setItem("alerts", JSON.stringify(alerts));
+  } catch (error) {
+    console.error("Error saving alert:", error);
+  }
+}
+
+export function getAlerts() {
+  try {
+    return JSON.parse(localStorage.getItem("alerts") || "[]");
+  } catch (error) {
+    console.error("Error getting alerts:", error);
+    return [];
+  }
+}
+
+export function removeAlerts() {
+  try {
+    localStorage.removeItem("alerts");
+  } catch (error) {
+    console.error("Error removing alerts:", error);
+  }
+}
+
+export function clearAll() {
+  try {
+    localStorage.clear();
+  } catch (error) {
+    console.error("Error clearing local storage:", error);
   }
 }
