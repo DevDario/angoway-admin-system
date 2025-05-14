ed30c6965b7dde4fa2d60e555e025db27ed8801e
Dário Silva <dariosilva13222@gmail.com>
2025-05-14 23:24:37 +0100

    feat: enhance alerts management by initializing stored alerts on hook mount

--- hooks/useAlertsChannel.ts
+++ hooks/useAlertsChannel.ts
@@ -1,34 +1,47 @@
 import { useEffect, useState } from "react";
 import { socket } from "../api/socket-instance";
 import { AlertNotification } from "../types/alert.notification";
+import { saveAlert, getAlerts } from "../utils/secure-store";
 
 export function useAlertsChannel() {
   const [alerts, setAlerts] = useState<AlertNotification[]>([]);
   const [recentAlert, setRecentAlert] = useState<AlertNotification>();
 
   useEffect(() => {
+    (async () => {
+      const storedAlerts = getAlerts();
+      if (storedAlerts) setAlerts(storedAlerts);
+    })();
+  }, []);
+
+  useEffect(() => {
     if (!socket.connected) socket.connect();
 
     function handleAlertsUpdate(data: AlertNotification) {
-      //debug
-      console.log("received alert update: ", data);
-
       setRecentAlert(data);
 
       setAlerts((prev) => {
         const index = prev.findIndex(
           (alert) => alert.driverId === data.driverId
         );
+        let updated;
         if (index !== -1) {
-          const updated = [...prev];
+          updated = [...prev];
           updated[index] = data;
-          return updated;
+        } else {
+          updated = [...prev, data];
         }
-        return [...prev, data];
+        
+        saveAlert(data); 
+        return updated;
       });
     }
 
     socket.on("alertNotificationsUpdate", handleAlertsUpdate);
+
+    return () => {
+      socket.off("alertNotificationsUpdate", handleAlertsUpdate);
+    };
   }, []);
 
   return {
