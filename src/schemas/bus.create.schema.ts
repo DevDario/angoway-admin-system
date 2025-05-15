836f19d28271ba8299e4c23d52a4df87a72ffc27
Dário Silva <dariosilva13222@gmail.com>
2025-05-15 20:25:54 +0100

    fix: sets rota and motorista attributes as ID (because the backend expects ID's and not texts)

--- src/schemas/bus.create.schema.ts
+++ src/schemas/bus.create.schema.ts
@@ -6,7 +6,9 @@ export const createBusSchema = z.object({
     .max(11, "A matrícula deve ter no máximo 11 caracteres")
     .min(11, "A matrícula deve ter no mínimo 11 caracteres")
     .nonempty("Você precisa informar a matrícula"),
-  rota: z.string().nonempty("Você precisa informar uma rota"),
+  rota: z
+    .string(z.number().int().positive())
+    .nonempty("Você precisa informar uma rota"),
   capacidade: z.string(
     z
       .number()
@@ -15,14 +17,9 @@ export const createBusSchema = z.object({
       .min(20, "A capacidade deve ser de no mínimo 20 assentos")
       .positive()
   ),
-  // .number()
-  // .int("Apenas valores inteiros são válidos")
-  // .max(50, "A capacidade máxima é 50")
-  // .nonnegative("O valor deve ser positivo")
   motorista: z
-    .string()
-    .min(3, "O nome deve ter pelo menos 3 caracteres")
-    .nonempty("Você precisa informar o nome do motorista"),
+    .string(z.number().int().positive("o ID precisa ser um valor positivo"))
+    .optional(),
 });
 
 export type DriverCreateInput = z.infer<typeof createBusSchema>;
