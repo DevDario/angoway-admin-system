0b699812251eb01b191fa6e41f160a330627911f
Dário Silva <dariosilva13222@gmail.com>
2025-05-23 12:12:17 +0100

    feat: add regex validation for bus matricula format

--- src/schemas/bus.create.schema.ts
+++ src/schemas/bus.create.schema.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 export const createBusSchema = z.object({
   matricula: z
     .string()
+    .regex(
+      /^([A-Z]{2}-\d{2}-\d{2}-[A-Z]{2}|[A-Z]{3}-\d{3}-[A-Z]{2}|[A-Z]{2}-\d{3}-[A-Z]{2})$/
+    ,"Formato inválido. Exemplo: AB-12-34-CD ou ABC-123-DE ou AB-123-DE")
     .max(11, "A matrícula deve ter no máximo 11 caracteres")
     .min(11, "A matrícula deve ter no mínimo 11 caracteres")
     .nonempty("Você precisa informar a matrícula"),
