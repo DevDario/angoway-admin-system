ddb7296e74800e4cf99539811c29258e8c716898
Dário Silva <dariosilva13222@gmail.com>
2025-05-29 22:24:23 +0800

    fix: adds id prop

--- pages/route/RoutesPage.tsx
+++ pages/route/RoutesPage.tsx
@@ -77,6 +77,7 @@ export default function RoutesPage() {
             routes.map((route:RoutePreviewResponse, index) => (
               <RoutePreviewCard
                 key={index}
+                id={route.id}
                 name={route.name}
                 destination={route.destination}
                 origin={route.origin}
